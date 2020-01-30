d85af28fdd488fae8dc6b7beebc0f97d25e5b955
SamuraiViking <nelson67@stolaf.edu>
2020-01-30 00:50:57 -0600

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChessBoardComponent } from './chess-board/chess-board.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'play',
+    component: ChessBoardComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/play',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true }
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
